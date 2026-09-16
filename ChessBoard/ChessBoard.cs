92615d10edeab4e496cbe26d24c120ef573f4667
victor ebbhagen <ebbhagen1@gmail.com>
2026-09-16 14:27:53 +0200

    Add Modulo%

--- ChessBoard/ChessBoard.cs
+++ ChessBoard/ChessBoard.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 
-public class ChessBoard
+public class ChessBoard //Klass Chessboard (När vi skapar ett objekt i Program.cs skickar vi med nummer via konstruktor
 {
-	public ChessBoard
-	{
-		for (int rad = 3; < rad 50; rad++) //yttreloopen som går rad för rad upp till 50 (höjden)
+	public ChessBoard(int size) //Konstruktor tar emot storleken som en parameter 
+	{ 
+		
+		for (int rad = 0; rad < size, rad++) //yttrelopp för rader baserat på användarens inmatning 
 		{
-			for (int kolumn = 3; < kolumn 50; kolumn++) //inre loop för aktuella raden (bredden)
+			for (int kolumn = 0; kolumn < size; kolumn++) //inre loop (Kolumn är bredden på schackbrädet)
 			{
-				if (rad + kolumn % 2 == 0) ; // Om rad och kolumn är jämna, skriv ut en X ruta, ananrs 0
+				//Modulo kan räkna ut schackmönstret oavsett vilken storlek användaren väljer
+				if ((rad + kolumn) % 2 == 0) // Om rad och kolumn är jämna, skriver programemet ut en X ruta, ananrs 0
 
 				{
-					Console.writeline("X"); //Skriver ut X 
+				Console.Write(" X "); 
 
 				}
 				 else
 				{
-					Console.WriteLine("0"); // Skriver ut 0
+					Console.Write(" 0 "); 
 				}
 			}
+			Console.WriteLine(); //Hoppar till nästa rad när kolumnerna är klara
 		}
 	}
    
