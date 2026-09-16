46ccacf7fee5ab632117372169f0236bbba8a037
victor ebbhagen <ebbhagen1@gmail.com>
2026-09-16 10:48:50 +0200

    Add for-loop

--- ChessBoard/ChessBoard.cs
+++ ChessBoard/ChessBoard.cs
@@ -2,13 +2,25 @@
 
 public class ChessBoard
 {
-	public ChessBoard()
+	public ChessBoard
 	{
+		for (int rad = 3; < rad 50; rad++) //yttreloopen som går rad för rad upp till 50 (höjden)
+		{
+			for (int kolumn = 3; < kolumn 50; kolumn++) //inre loop för aktuella raden (bredden)
+			{
+				if (rad + kolumn % 2 == 0) ; // Om rad och kolumn är jämna, skriv ut en X ruta, ananrs 0
 
+				{
+					Console.writeline("X"); //Skriver ut X 
+
+				}
+				 else
+				{
+					Console.WriteLine("0"); // Skriver ut 0
+				}
+			}
+		}
 	}
-    public static int ReadSize()
-    {
-        Console.WriteLine("Ange storleken på schackbrädet 3 - 50. "); // Vi skriver ut en fråga till använden om att ge en siffra mellan 3 -50. 
-        string input = Console.ReadLine();
-    }
+   
+   
 }
--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -2,8 +2,16 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) //Startpunkten
     {
-        
-    }
+        {
+            Console.WriteLine("Ange storleken på schackbrädet 3 - 50. "); // Vi skriver ut en fråga till använden om att ge en siffra mellan 3 -50. 
+            string input = Console.ReadLine(); //VÄntar på att användaren ska skriva in siffror
+
+
+            if int.TryParse(input.out int nummer 3 && 50;); //Programmet kommer inte att krascha om man skriver text istället för siffra
+            {
+                Console.WriteLine("Fel inmatning, vad god ange en siffra"); //Skickar ut ett meddelande till användaren att skriva en siffra
+            }
+        }
 }
