00aaa8b86dda99c139f7045337634aefe723ccf6
victor ebbhagen <ebbhagen1@gmail.com>
2026-09-16 16:10:02 +0200

    Add RenderBoard

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,28 +1,33 @@
-﻿namespace ChessBoard;
+﻿using System;
 
-
-class Program
+namespace ChessBoard
 {
-    static void Main(string[] args) //Startpunkten
+
+    class Program
     {
-        int nummer; //number måste ligga utanför loopen för att använda senare
-
-        while (true) // Vi Loopar till vi bryter oss ut med "Break" //
-                     // när vi inte vet hur många försök det kommer ta
+        static void Main(string[] args) //Startpunkten
         {
-            Console.WriteLine("Ange storleken på schackbrädet 3 - 50. "); // Vi skriver ut en fråga till använden om att ge en siffra mellan 3 -50. 
-            string input = Console.ReadLine(); //VÄntar på att användaren ska skriva in siffror
+            int nummer; //number måste ligga utanför loopen för att använda senare
 
-            // om gilitig, då går det att tolka som ett tal, det är minst 3, och det är max 50 
-            if (Int.TryParse(input, out nummer)&& nummer >= 3 && nummer <= 50) //Programmet kommer inte att krascha om man skriver text istället för siffra
+            while (true) // Vi Loopar till vi bryter oss ut med "Break" //
+                         // när vi inte vet hur många försök det kommer ta
             {
-                break; // Giligt tal, den hoppar ut ur while loopen
-            }
+                Console.WriteLine("Ange storleken på schackbrädet 3 - 50. "); // Vi skriver ut en fråga till använden om att ge en siffra mellan 3 -50. 
+                string input = Console.ReadLine(); //VÄntar på att användaren ska skriva in siffror
 
-            Console.WriteLine ("Fel inmatning, vad god ange en siffra"); //Skickar ut ett meddelande till användaren att skriva en siffra
+                // om gilitig, då går det att tolka som ett tal, det är minst 3, och det är max 50 
+                if (int.TryParse(input, out nummer) && nummer >= 3 && nummer <= 50) //Programmet kommer inte att krascha om man skriver text istället för siffra
+                {
+                    break; // Giligt tal, den hoppar ut ur while loopen
+                }
+
+                Console.WriteLine("Fel inmatning, vad god ange en siffra"); //Skickar ut ett meddelande till användaren att skriva en siffra
+            }
+            RenderBoard myBoard = new RenderBoard(nummer); // skapar schackbrädet - när vi anropar konstruktorn med användarens siffra.
         }
-    }   
+    }
 }
+
 
     //Refaktuering - fixar/städar upp koden så att den blir lättare att förstå. Byta namn, ta bort dubbleter och långa metoder
     //Merge konflikt - Att inte ändra medans den andra kodar. Svårt att läsa in koden igen, kunde undo via "git changes" och sen hela filer. 
---
+++ ChessBoard/RenderBoard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChessBoard
+{
+	public class RenderBoard //Klass Chessboard (När vi skapar ett objekt i Program.cs skickar vi med nummer via konstruktor
+	{
+		public RenderBoard(int size) //Konstruktor tar emot storleken som en parameter 
+		{
+
+			for (int rad = 0; rad < size; rad++)  //yttrelopp för rader baserat på användarens inmatning 
+			{
+				for (int kolumn = 0; kolumn < size; kolumn++) //inre loop (Kolumn är bredden på schackbrädet)
+				{
+					//Modulo kan räkna ut schackmönstret oavsett vilken storlek användaren väljer
+					if ((rad + kolumn) % 2 == 0) // Om rad och kolumn är jämna, skriver programemet ut en X ruta, ananrs 0
+
+					{
+						Console.Write(" X ");
+
+					}
+					else
+					{
+						Console.Write(" 0 ");
+					}
+				}
+				Console.WriteLine(); //Hoppar till nästa rad när kolumnerna är klara
+			}
+		}
+	}
+}
