b58be7785f00cc84eaae83694afc26b6681a5494
victor ebbhagen <ebbhagen1@gmail.com>
2026-09-16 08:41:29 +0200

    Add Readsize

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Net.NetworkInformation;
+
+namespace ChessBoard
+{
+    public class Chessboard
+    {
+        public static int ReadSize()
+        {
+            Console.WriteLine("Ange storleken på schackbrädet 3 - 50. "); // Vi skriver ut en fråga till använden om att ge en siffra mellan 3 -50. 
+            string input = Console.ReadLine();
+        }
+
+
+
+    }
+}
