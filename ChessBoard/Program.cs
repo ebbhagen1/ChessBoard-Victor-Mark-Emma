12b4f1994fc48509172193b3389ff967536a155d
mark assira <markassira@hotmail.com>
2026-09-16 15:14:14 +0200

    Add while loop

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -5,18 +5,23 @@ class Program
 {
     static void Main(string[] args) //Startpunkten
     {
-        int number; //number måste ligga utanför loopen för att använda senare
+        int nummer; //number måste ligga utanför loopen för att använda senare
 
+        while (true) // Vi Loopar till vi bryter oss ut med "Break" //
+                     // när vi inte vet hur många försök det kommer ta
         {
             Console.WriteLine("Ange storleken på schackbrädet 3 - 50. "); // Vi skriver ut en fråga till använden om att ge en siffra mellan 3 -50. 
             string input = Console.ReadLine(); //VÄntar på att användaren ska skriva in siffror
 
-
-            if int.TryParse(input.out int nummer 3 && 50;); //Programmet kommer inte att krascha om man skriver text istället för siffra
+            // om gilitig, då går det att tolka som ett tal, det är minst 3, och det är max 50 
+            if (Int.TryParse(input, out nummer)&& nummer >= 3 && nummer <= 50) //Programmet kommer inte att krascha om man skriver text istället för siffra
             {
-                Console.WriteLine("Fel inmatning, vad god ange en siffra"); //Skickar ut ett meddelande till användaren att skriva en siffra
+                break; // Giligt tal, den hoppar ut ur while loopen
             }
+
+            Console.WriteLine ("Fel inmatning, vad god ange en siffra"); //Skickar ut ett meddelande till användaren att skriva en siffra
         }
+    }   
 }
 
     //Refaktuering - fixar/städar upp koden så att den blir lättare att förstå. Byta namn, ta bort dubbleter och långa metoder
