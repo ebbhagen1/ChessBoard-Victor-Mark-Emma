9b8ff56c97bb223562d50392737bce052cfb3c54
mark assira <markassira@hotmail.com>
2026-09-18 03:32:29 +0200

    Add Readread text

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Spectre.Console;
-using chessBoard;
+using ChessBoard;
 
 namespace ChessBoard
 {
@@ -35,7 +35,7 @@ namespace ChessBoard
                     Console.WriteLine("Fel inmatning, vad god ange en siffra"); //Skickar ut ett meddelande till användaren att skriva en siffra
                 }
                 var myBoard = new RenderBoard(nummer); // skapar schackbrädet - när vi anropar konstruktorn med användarens siffra.
-                myboard.Render();
+                myBoard.Render();
             }
         }
     }
